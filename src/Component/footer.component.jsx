41299c208c6b0a6892eda2551074f0bb3a6b234b
kanavmittal <kanavmittal0@gmail.com>
2021-08-30 22:52:29 +0530

    fixed footer image

--- src/Component/footer.component.jsx
+++ src/Component/footer.component.jsx
@@ -9,7 +9,7 @@ class Footer extends React.Component {
                 </div>
                 <div className="w-full relative">
                     <img className="object-cover" src={footer} alt="" />
-                    <img className="absolute left-0 bottom-1/4 right-0 ml-auto mr-auto  h-60 md:h-96 "src={teamimg} alt="" />
+                    <img className="absolute left-0 bottom-1/4 right-0 ml-auto mr-auto  h-44 md:h-96 "src={teamimg} alt="" />
                     <p className="absolute top-0 md:top-3 mr-auto ml-auto left-12 md:left-44 lg:left-80 font-bold md:text-2xl">Kanav Mittal</p>
                     <p className="absolute bottom-0 top-16 md:top-32 mr-auto ml-auto left-8 md:left-36 lg:left-60 font-bold md:text-2xl">Samank Gupta</p>
                     <p className="absolute top-0 md:top-3 bottom-0 right-8 md:right-60 lg:right-80 font-bold md:text-2xl">Sarthak Tyagi</p>
